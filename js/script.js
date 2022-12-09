c0e69da106490f096f9a51f1464a10dc59813863
stanleyos <stanislaw.sarka@gmail.com>
2022-12-09 08:59:27 +0100

    script.js - algorithm description added

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// document.getElementById('my-button').addEventListener('click', () => {
+//   const links = document.querySelectorAll('.titles a')
+//   console.log(links)
+// })
+
+const titleClickHandler = (e) => {
+  console.log(e)
+
+  /* remove class 'active' from all article links  */
+
+  /* add class 'active' to the clicked link */
+
+  /* remove class 'active' from all articles */
+
+  /* get 'href' attribute from the clicked link */
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+
+  /* add class 'active' to the correct article */
+}
+
+const links = document.querySelectorAll('.titles a')
+
+for (let link of links) {
+  link.addEventListener('click', titleClickHandler)
+}
