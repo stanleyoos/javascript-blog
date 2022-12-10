4e64e387dcfdb8c7f1783e0800af5b84ea705220
stanleyos <stanislaw.sarka@gmail.com>
2022-12-10 10:55:46 +0100

    added functionality to remove .active from links and articles

--- js/script.js
+++ js/script.js
@@ -6,13 +6,25 @@
 //
 
 const titleClickHandler = (e) => {
-  console.log(e)
+  //console.log(e)
 
   /* remove class 'active' from all article links  */
+
+  const activeLinks = document.querySelectorAll('.titles a.active')
+
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove('active')
+  }
 
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
+
+  const activeArticles = document.querySelectorAll('article.active')
+
+  for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove('active')
+  }
 
   /* get 'href' attribute from the clicked link */
 
