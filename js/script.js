989cfb938670a36ccf4c7bac93194bf5edbb3613
stanleyos <stanislaw.sarka@gmail.com>
2022-12-10 12:19:13 +0100

    titleClickHandler is done - all articles works properly

--- js/script.js
+++ js/script.js
@@ -4,33 +4,39 @@
 // })
 
 //
+'use strict'
 
-const titleClickHandler = (e) => {
-  //console.log(e)
+const titleClickHandler = function (e) {
+  e.preventDefault()
+  const clickedElement = this
 
-  /* remove class 'active' from all article links  */
-
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active')
 
   for (let activeLink of activeLinks) {
     activeLink.classList.remove('active')
   }
 
-  /* add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
+  clickedElement.classList.add('active')
+  //console.log('clicked element: ', this)
 
-  /* remove class 'active' from all articles */
-
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('article.active')
 
   for (let activeArticle of activeArticles) {
     activeArticle.classList.remove('active')
   }
 
-  /* get 'href' attribute from the clicked link */
+  /* [DONE] get 'href' attribute from the clicked link */
+  const attribute = clickedElement.getAttribute('href')
+  //console.log(attribute)
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+  const correctArticle = document.querySelector(attribute)
 
-  /* add class 'active' to the correct article */
+  /* [DONE] add class 'active' to the correct article */
+  correctArticle.classList.add('active')
 }
 
 const links = document.querySelectorAll('.titles a')
