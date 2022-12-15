4af5e81d460aa49139705b97eb7c7bf88fb928e9
stanleyos <stanislaw.sarka@gmail.com>
2022-12-15 16:39:46 +0100

    links are now generated with listener: titleClickHandler

--- js/script.js
+++ js/script.js
@@ -39,8 +39,45 @@ const titleClickHandler = function (e) {
   correctArticle.classList.add('active')
 }
 
-const links = document.querySelectorAll('.titles a')
+//////////////////////
 
-for (let link of links) {
-  link.addEventListener('click', titleClickHandler)
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles'
+
+function generateTitleLinks() {
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector)
+  titleList.innerHTML = ''
+  /* for each article */
+  const articles = document.querySelectorAll(optArticleSelector)
+
+  let html = ''
+  for (let article of articles) {
+    /* get the article id */
+    const articleId = article.getAttribute('id')
+    /* find the title element */
+    /* get the title from the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML
+
+    /* create HTML of the link */
+    const linkHTML =
+      '<li><a href="#' +
+      articleId +
+      '"><span>' +
+      articleTitle +
+      '</span></a></li>'
+    console.log(linkHTML)
+    /* insert link into titleList */
+    html += linkHTML
+  }
+  titleList.innerHTML = html
+
+  const links = document.querySelectorAll('.titles a')
+
+  for (let link of links) {
+    link.addEventListener('click', titleClickHandler)
+  }
 }
+
+generateTitleLinks()
